a73d0ba88116ec922601ac7e91c124b341559a21
Serenity <d.palbanese@gmail.com>
2020-01-01 19:40:05 +0100

    cant rename files so maybe i can copy it?

--- testE2E.sh
+++ testE2E.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-mv ./CDN.NET.Backend/appsettings.json.example ./CDN.NET.Backend/appsettings.json
+cp ./CDN.NET.Backend/appsettings.json.example ./CDN.NET.Backend/appsettings.json
 dotnet run -p ./CDN.NET.Backend &
 server=$!
 sleep 10
 dotnet test
 kill $server
-mv ./CDN.NET.Backend/appsettings.json ./CDN.NET.Backend/appsettings.json.example
